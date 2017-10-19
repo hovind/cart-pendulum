46f2aa1a0ca91a48d12f8a53ee77998e0ebcf22b
Øystein Hovind <oysthovi@ntnu.no>
2017-10-19 18:02:36 +0200

    Made something somewhat working.

--- plant.m
+++ plant.m
@@ -10,19 +10,24 @@ config = init_animate_cart(width, height, length);
 
 
 t = 0;
-theta = 0.1;
+theta = pi+0.01;
 dtheta = 0;
 ddtheta = 0;
-x = 0;
+
+x = -pi;
 dx = 0;
 ddx = 0;
+
 for i = 1:N,
     t = t + h;
     
-    f = 0;
+    f = - 2*g*sin(theta)/(1 + cos(theta)) + cos(theta)*ddtheta - sin(theta)*dtheta^2; 
     
-    ddx = (f + sin(theta)*dtheta^2 - cos(theta)*ddtheta)/2;
-    ddtheta = g*sin(theta) - cos(theta)*ddx;
+    %ddx = (f + sin(theta)*dtheta^2 - cos(theta)*ddtheta)/2;
+    %ddtheta = g*sin(theta) - cos(theta)*ddx;
+    ddx = 1/(2 - cos(theta)^2)*(f + sin(theta)*dtheta^2 - g*cos(theta)*sin(theta));
+    ddtheta = 1/(2 - cos(theta)^2)*(2*g*sin(theta) - cos(theta)*sin(theta)*dtheta^2 - cos(theta)*f);
+    
     
     dx = dx + h*ddx;
     dtheta = dtheta + h*ddtheta;
@@ -31,5 +36,4 @@ for i = 1:N,
     theta = theta + h*dtheta;
     
     animate_cart(config, x, theta);
-    %ddx = - g*sin(x)/(1 + cos(x));
 end
